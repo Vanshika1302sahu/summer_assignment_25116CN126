9b84e79fa73fca2778ad3e1f6944d3e25757d14d
Vanshika1302sahu <sahusarojlata@gmail.com>
2026-05-28 21:55:08 +0530

    Add files via upload

---
+++ DAY 1/t4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num,i,fact=1;
+    
+    printf("enter a number:");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    {
+        fact=fact*i;
+    }
+    printf("the factorial of %d is %d",num,fact);
+    return 0;
+}
