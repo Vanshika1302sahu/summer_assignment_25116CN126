6ffc153361b5f5d3677b4af1ed30e9aea1bd48c9
Vanshika1302sahu <sahusarojlata@gmail.com>
2026-06-18 20:26:14 +0530

    Add files via upload

---
+++ DAY15/q4.c
@@ -0,0 +1,32 @@
+//Move all zeros to end.
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, j = 0;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] != 0)
+        {
+            a[j] = a[i];
+            j++;
+        }
+    }
+
+    while (j < n)
+    {
+        a[j] = 0;
+        j++;
+    }
+
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
