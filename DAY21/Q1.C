30c81d9d393a745cad44038d69e24f7cdd8f273d
Vanshika1302sahu <sahusarojlata@gmail.com>
2026-06-24 20:57:48 +0530

    Add files via upload

---
+++ DAY21/Q1.C
@@ -0,0 +1,16 @@
+//String length without strlen()
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count=0;
+
+    gets(str);
+
+    while(str[count]!='\0')
+        count++;
+
+    printf("%d",count);
+
+    return 0;
+}
